da9003f7ba2f971c1a0127d0ba7d6d0c60ac6be6
Andry <lada012012@mail.ru>
2013-05-17 10:10:31 +0400

    Вывод каталога для раздела подготовка к свадьбе

--- bitrix/components/mar/newlyweds_page/templates/.default/forms/newlyweds.php
+++ bitrix/components/mar/newlyweds_page/templates/.default/forms/newlyweds.php
@@ -11,21 +11,21 @@
     $current_user = CUser::GetById($USER->GetID())->Fetch();
 
     ?>
-<!--    --><?//
-//    echo "<pre>";
-//    echo print_r($current_user);
-//    echo "</pre>";
-//    ?>
+    <!--    --><?//
+    //    echo "<pre>";
+    //    echo print_r($current_user);
+    //    echo "</pre>";
+    //    ?>
 
-<!--    <script type="text/javascript">-->
-<!--        $(function(){-->
-<!--            if($('.input-pop').val() != ""){-->
-<!--                $('.red_col').css({-->
-<!--                   'background' : '#56A108'-->
-<!--                });-->
-<!--            }-->
-<!--        });-->
-<!--    </script>-->
+    <!--    <script type="text/javascript">-->
+    <!--        $(function(){-->
+    <!--            if($('.input-pop').val() != ""){-->
+    <!--                $('.red_col').css({-->
+    <!--                   'background' : '#56A108'-->
+    <!--                });-->
+    <!--            }-->
+    <!--        });-->
+    <!--    </script>-->
 
 
     <form action="" method="post">
@@ -36,9 +36,6 @@
                    class="input-pop enable"/>
             <!--            --><?//=$arResult['errors']['Имя невесты'][0]?>
         </div>
-
-
-
 
 
         <div class="form-box-2">
@@ -63,7 +60,6 @@
         </div>
 
 
-
         <div class="form-box-2">
             <span>Телефон:</span>
             <input size="30" type="text" name="phone_Groom" value="<?= $current_user['PERSONAL_PHONE'] ?>"
@@ -76,44 +72,44 @@
         </div>
 
 
-                <h2 class="new">Жених:</h2>
+        <h2 class="new">Жених:</h2>
 
 
-                <div class="form-box-2">
-                    <span>Имя:<i>*</i></span>
-                    <input size="30" type="text" name="name_Bride" value="" class="input-pop enable"/>
-                    <?=$arResult['errors']['Имя жениха'][0]?>
-                </div>
+        <div class="form-box-2">
+            <span>Имя:<i>*</i></span>
+            <input size="30" type="text" name="name_Bride" value="" class="input-pop enable"/>
+            <?=$arResult['errors']['Имя жениха'][0]?>
+        </div>
 
-                <div class="form-box-2">
-                    <span>Фамилия:</span>
-                    <input size="30" type="text" name="surname_Bride" value="" class="input-pop enable"/>
-                    <?=$arResult['errors']['Фамилия жениха'][0]?>
-                </div>
+        <div class="form-box-2">
+            <span>Фамилия:</span>
+            <input size="30" type="text" name="surname_Bride" value="" class="input-pop enable"/>
+            <?=$arResult['errors']['Фамилия жениха'][0]?>
+        </div>
 
-                <div class="form-box-2">
-                    <span>E-mail:<i>*</i></span>
-                    <input size="30" class="input-pop enable" type="text" name="email_Bride" value="" autocomplete="off"/>
-                    <?=$arResult['errors']['Email жениха'][0]?>
-                </div>
+        <div class="form-box-2">
+            <span>E-mail:<i>*</i></span>
+            <input size="30" class="input-pop enable" type="text" name="email_Bride" value="" autocomplete="off"/>
+            <?=$arResult['errors']['Email жениха'][0]?>
+        </div>
 
-                <div class="form-box-2">
-                    <span>Город:<i>*</i></span>
+        <div class="form-box-2">
+            <span>Город:<i>*</i></span>
 
-                    <?=SelectBoxFromArray('town_Bride', GetCitys(), 'class="select-2" onchange="$(this).closest(\'form\').trigger(\'submit\');"');?>
+            <?=SelectBoxFromArray('town_Bride', GetCitys(), 'class="select-2" onchange="$(this).closest(\'form\').trigger(\'submit\');"');?>
 
-                    <?=$arResult['errors']['Город жениха'][0]?>
-                </div>
+            <?=$arResult['errors']['Город жениха'][0]?>
+        </div>
 
-                <div class="form-box-2">
-                    <span>Телефон:</span>
-                    <input size="30" type="text" name="phone_Bride" value="" class="input-pop"/>
-                </div>
-                <div class="form-box-2">
-                    <span>Скайп:</span>
-                    <input size="30" type="text" name="skype_Bride" value=""
-                           class="input-pop"/>
-                </div>
+        <div class="form-box-2">
+            <span>Телефон:</span>
+            <input size="30" type="text" name="phone_Bride" value="" class="input-pop"/>
+        </div>
+        <div class="form-box-2">
+            <span>Скайп:</span>
+            <input size="30" type="text" name="skype_Bride" value=""
+                   class="input-pop"/>
+        </div>
         <!---->
         <div style="text-align: right; margin-bottom: 60px">
             <input type="submit" class="button marg" name="submit" value="Сохранить"/>
--- bitrix/components/mar/newlyweds_page/templates/.default/forms/wedding_date.php
+++ bitrix/components/mar/newlyweds_page/templates/.default/forms/wedding_date.php
@@ -27,13 +27,6 @@
                     )
                 );?>
 
-                <!--                <input type="text" name="UF_WED_DATE" value="" class="fields datetime">-->
-                <!---->
-                <!--                <img src="/bitrix/js/main/core/images/calendar-icon.gif" alt="Выбрать дату в календаре"-->
-                <!--                     class="calendar-icon"-->
-                <!--                     onclick="BX.calendar({node:this, field:'UF_WED_DATE', form: 'regform', bTime: true, currentTime: '1366630060', bHideTime: false});"-->
-                <!--                     onmouseover="BX.addClass(this, 'calendar-icon-hover');"-->
-                <!--                     onmouseout="BX.removeClass(this, 'calendar-icon-hover');" border="0"/>-->
             </div>
         </div>
 
@@ -45,5 +38,204 @@
     <h3 class="statii">Возможно вам будут интересны статьи:</h3>
 
     <?=getStats::getStatsFunction(19);?>
+    <?
 
+    $GROUP_ID = 7;
+
+    if(!CModule::IncludeModule("iblock"))
+    {
+        ShowError(GetMessage("IBLOCK_MODULE_NOT_INSTALLED"));
+        return;
+    }
+
+    $arDefaultVariableAliases404 = array();
+
+    $SEF_URL_TEMPLATES = array(
+        "catalog" => "#STATUS_CODE#/"
+    );
+
+    $arUrlTemplates = CComponentEngine::MakeComponentUrlTemplates($arDefaultVariableAliases404, $SEF_URL_TEMPLATES);
+    $arVariables = array();
+    $componentPage = CComponentEngine::ParseComponentPath(
+        "/catalog/",
+        $arUrlTemplates,
+        $arVariables
+    );
+
+    $obCache = new CPHPCache;
+    $life_time = 30*60; // ����� ����������� - 30 �����
+    $city_id = GetDefCity();
+    $cache_id = 'CATALOG_LIST_STATUS_COUNTS_' . $city_id;
+    if($obCache->InitCache($life_time, $cache_id, "/"))
+    {
+        $counts = $obCache->GetVars();
+    }
+
+    // �������� ��������������� ������
+    if($obCache->StartDataCache())
+    {
+        $strSql = "
+		SELECT count(uts.VALUE_ID) as ct
+				 , uts.UF_STATUS as st
+		FROM
+			b_uts_user AS uts, b_user_group AS b_group
+		WHERE
+			GROUP_ID = $GROUP_ID
+			AND VALUE_ID = USER_ID
+			AND UF_CITY = $city_id
+		GROUP BY
+			uts.UF_STATUS
+	";
+
+        $res = $DB->Query($strSql, true);
+        $counts = array();
+        while($count = $res->GetNext())
+        {
+            $counts[(int)$count['st']] = (int)$count['ct'];
+        }
+        $obCache->EndDataCache($counts);
+    }
+
+    $arSelect = array(
+        'ID','NAME','CODE','PROPERTY_A_NAMES','PROPERTY_A_TITLE'
+    );
+    $statusFilter = array(
+        "IBLOCK_ID" => 3,
+        "ACTIVE" => "Y",
+    );
+    $arResult['STATUS']= array();
+    $statuses = CIBlockElement::GetList(array("SORT"=>"ASC"),$statusFilter, false, false, $arSelect);
+    while($status = $statuses->GetNext())
+    {
+        if($status['CODE'] == $arVariables['STATUS_CODE'])
+        {
+            $arResult['STATUS_SELECT'] = $status;
+            $status['SELECT'] = "Y";
+            if($status['PROPERTY_A_TITLE_VALUE'])
+                CMar::SetTitle($status['PROPERTY_A_TITLE_VALUE']);
+        }
+        if(isset($counts[(int)$status['ID']]))
+        {
+            $status['USER_COUNT'] = $counts[(int)$status['ID']];
+        }
+        $arResult['STATUS'][] = $status;
+    }
+
+    $arParam["SELECT"][]="UF_RATING";
+
+    $filter = Array
+    (
+        "ACTIVE" => "Y",
+        "UF_CITY" => GetDefCity(),
+        "GROUPS_ID" => Array($GROUP_ID)
+    );
+
+    if($componentPage == 'catalog')
+    {
+        $filter['UF_STATUS'] = $arResult['STATUS_SELECT']['ID'];
+    }
+
+    if(!empty($_GET['search'])){
+        unset($filter['UF_CITY']);
+        $name = strip_tags($_GET['search']);
+        $name = explode(' ', $name);
+        foreach($name as &$search) {
+            $search .= '%';
+        }
+        $filter['NAME'] = '('.implode(' && ', $name).')';
+    }
+
+    $rsUsers = CUser::GetList(($by="UF_RATING"), ($order="desc"), $filter, $arParam);
+    $is_filtered = $rsUsers->is_filtered;
+    $rsUsers->NavStart(3);
+
+    $arResult['itemsCount'] = $rsUsers->NavRecordCount;
+    $arResult['items'] = array();
+
+    while($arUser = $rsUsers->Fetch())
+    {
+        if($arFile = CFile::ResizeImageGet($arUser['PERSONAL_PHOTO'], array('width'=>62, 'height'=>62), BX_RESIZE_IMAGE_EXACT, true))
+        {
+            $arUser['PERSONAL_PHOTO'] = $arFile['src'];
+        }
+
+        $arUser['RATING'] = intval($arUser['UF_RATING']);
+
+        $arFilter = array(
+            'IBLOCK_ID' => 1,
+            'CREATED_BY' => $arUser['ID'],
+        );
+
+        $opPhoto = CIBlockElement::GetList(Array(), $arFilter, false, array("nTopCount" => 3));
+
+        $arUser['PHOTOS'] = array();
+
+        while($arPhoto = $opPhoto->GetNext())
+        {
+            if($arPhoto['PREVIEW_PICTURE'] && $arFile = CFile::ResizeImageGet($arPhoto['PREVIEW_PICTURE'], array('width'=>121, 'height'=>121), BX_RESIZE_IMAGE_EXACT, true))
+            {
+                $arPhoto['PICTURE'] = $arFile['src'];
+            }
+
+            $arUser['PHOTOS'][] = $arPhoto;
+        }
+        // ���������� �����
+        $arResult['items'][] = $arUser;
+    }
+
+//    $arResult['navigation'] = $rsUsers->GetNavPrint(
+//        GetMessage("PAGES"),
+//        false,
+//        "text",
+//        '/bitrix/components/mar/catalog.list/templates/.default/pagenavigation.php'
+//    );
+
+    ?>
+
+
+    <div class="content-right-2">
+
+        <div class="content-header">
+            <?=isset($arResult['STATUS_SELECT'])?$arResult['STATUS_SELECT']['PROPERTY_A_NAMES_VALUE']:''?>
+            <!--		<span>--><?//=$arResult['itemsCount']?><!--</span>-->
+        </div>
+        <div class="end"></div>
+
+        <? foreach($arResult['items'] as $arUser):  ?>
+            <!--	--><?// echo '<pre>', print_r($arUser, true) , '</pre>';?>
+            <div class="ispolnitel-name-container">
+                <div class="ispolnitel-name">
+                    <a class="catalog-box" href="/profile/<?=$arUser['ID']?>/">
+                        <?if($arUser['PERSONAL_PHOTO']):?>
+                            <img src="<?=$arUser['PERSONAL_PHOTO']?>" />
+                        <?else:?>
+                            <img src="<?=SITE_TEMPLATE_PATH ?>/images/cover_empty.jpg" width="61px" height="61px"/>
+                        <?endif;?>
+                        <span><?=$arUser['NAME']?> <?=$arUser['LAST_NAME']?></span>
+                        <u><?=$arUser['PERSONAL_CITY']?></u>
+                        Рейтинг: <i><?=$arUser['RATING']?></i>
+                        <b>&nbsp;</b>
+                    </a>
+                </div>
+                <?if($arUser['PHOTOS']):?>
+                    <div class="ispolnitel-prew">
+                        <?foreach($arUser['PHOTOS'] as $photo): ?>
+                            <a href="/profile/<?=$arUser['ID']?>/"><img src="<?=$photo['PICTURE']?>"/></a>
+                        <?endforeach;?>
+                        <div class="end"></div>
+                    </div>
+                <?endif;?>
+                <div class="end"></div>
+            </div>
+        <? endforeach; ?>
+
+        <?=$arResult['navigation'];?>
+
+    </div>
+
+<!--        --><?//
+//        echo "<pre>";
+//        print_r($arUser) ;
+//        echo "<pre>";
+//        ?>
 </div>
